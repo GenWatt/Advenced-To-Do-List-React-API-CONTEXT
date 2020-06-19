70b23d3b3fe4dbd05bb82dc3ae3711b7f7730f7d
GenWatt <adrian.nowy@op.pl>
2020-06-19 18:47:10 +0200

    Updates

---
+++ precache-manifest.35cef8c148fab0dbb4bedebbfe255e65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda76fff3d956c31970bd9c971aec7f1",
+    "url": "/Advenced-To-Do-List-React-API-CONTEXT/index.html"
+  },
+  {
+    "revision": "9ddcf6b9a93b76eab385",
+    "url": "/Advenced-To-Do-List-React-API-CONTEXT/static/css/main.b30ee6a1.chunk.css"
+  },
+  {
+    "revision": "37faf96cbeef86d0cd1a",
+    "url": "/Advenced-To-Do-List-React-API-CONTEXT/static/js/2.31736dc7.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/Advenced-To-Do-List-React-API-CONTEXT/static/js/2.31736dc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ddcf6b9a93b76eab385",
+    "url": "/Advenced-To-Do-List-React-API-CONTEXT/static/js/main.043eb24d.chunk.js"
+  },
+  {
+    "revision": "e81a159264523747c463",
+    "url": "/Advenced-To-Do-List-React-API-CONTEXT/static/js/runtime-main.ccfd4ffd.js"
+  }
+]);
